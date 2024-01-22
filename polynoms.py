ea1ab96f771a7c1da7164c10e5463ad32fe117f5
PiggyPig <98620482+PiggyPigCute@users.noreply.github.com>
2024-01-22 22:17:46 +0100

    🎲 polynoms

--- polynoms.py
+++ polynoms.py
@@ -19,14 +19,11 @@ class poly:
     def __setitem__(self, i, coeff):
         while i >= len(self): self.coeff.append(0)
         self.coeff[i] = coeff
-        while self[-1] == 0: self.pop()
         return self
     
     def __len__(self) -> int:
+        while self.coeff[-1] == 0: self.coeff.pop()
         return len(self.coeff)
-
-    def pop(self):
-        return self.coeff.pop()
     
     def __add__(self, other):
         return poly([self[i]+other[i] for i in range(max(len(self),len(other)))]) if isinstance(other,poly) else poly([self[0]+other]+self[1:len(self)]) # type: ignore
@@ -49,14 +46,26 @@ class poly:
     def __pow__(self, other:int):
         return (poly(1),self.copy,self*self)[other] if other<=2 else (self**((other-1)//2))**2*((other%2==0) + (other%2!=0)*self)
     
-    def get_deg(self):
-        return len(self) if len(self)>0 else float('-inf')
+    def __eq__(self, other) -> bool:
+        return self.coeff == other.coeff
+    
+    def __neq__(self, other) -> bool:
+        return not self == other
+
+    def get_deg(self) -> int|float:
+        return len(self)-1 if len(self)>0 else float('-inf')
+    
+    def get_max(self):
+        return self[self.deg]
     
     def get_copy(self):
         return poly(self.coeff.copy())
 
     deg = property(get_deg)
+    max = property(get_max)
     copy = property(get_copy)
 
 
 X = poly(0,1)
+X2 = poly(0,0,1)
+X3 = poly(0,0,0,1)
