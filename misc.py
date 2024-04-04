ec5ac1f1ddcb6c4b84e262da7d0460e9d07e9d04
PiggyPig <98620482+PiggyPigCute@users.noreply.github.com>
2024-04-04 21:29:18 +0200

    🪣 arithmetics in `misc`

--- misc.py
+++ misc.py
@@ -30,3 +30,24 @@ def rj():
     return rf() + 1j*rf()
 
 π = 3.14159265358979323
+
+def is_prime(n:int) -> bool:
+    if n < 2:
+        return False
+    for i in range(2,n-1):
+        if n%i == 0:
+            return False
+    return True
+
+def eratosthenes(n:int) -> list[bool]:
+    if n < 2:
+        return [False]*n
+    table = [i>1 for i in range(n)]
+    i = 2
+    while i**2 < n:
+        j = i*2
+        while j < n:
+            table[j] = False
+            j *= i
+        i += 1
+    return table
